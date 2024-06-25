fa3cb5c69529bb48822755967418d1bc879241c5
Thurston Yates <thurstonalexyates@gmail.com>
2024-06-25 15:57:37 -0400

    More LVGL display driver additions

--- frontend/include/display.hpp
+++ frontend/include/display.hpp
@@ -1,4 +1,5 @@
 #include "liblvgl/lvgl.h"
+#include <thread>
 #include <wx/wx.h>
 
 class LVGLDisplay : public wxPanel {
@@ -12,5 +13,13 @@ class LVGLDisplay : public wxPanel {
 	wxDECLARE_EVENT_TABLE();
 
   private:
-	void lvgl_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
+	static lv_disp_drv_t disp_drv;
+	static lv_disp_draw_buf_t disp_buf;
+	static lv_color_t buf1[LV_HOR_RES_MAX * 10];
+	static lv_color_t buf2[LV_HOR_RES_MAX * 10];
+
+	std::thread *tick_thread;
+
+	static void lvgl_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
+	static void lvgl_tick();
 };
--- frontend/src/display.cpp
+++ frontend/src/display.cpp
@@ -1,4 +1,5 @@
 #include "display.hpp"
+#include <chrono>
 #include <wx/event.h>
 
 // clang-format off
@@ -8,7 +9,27 @@ wxEND_EVENT_TABLE();
 // clang-format on
 
 LVGLDisplay::LVGLDisplay(wxWindow *parent)
-    : wxPanel(parent, wxID_ANY, wxPoint(0, 0), wxSize(480, 240)) {}
+    : wxPanel(parent, wxID_ANY, wxPoint(0, 0), wxSize(LV_HOR_RES_MAX, LV_VER_RES_MAX)) {
+	lv_init();
+
+	// Display Driver
+	lv_disp_draw_buf_init(&disp_buf, buf1, buf2, LV_HOR_RES_MAX * 10);
+	lv_disp_drv_init(&disp_drv);
+	disp_drv.draw_buf = &disp_buf;
+	disp_drv.flush_cb = lvgl_flush_cb;
+	disp_drv.hor_res = LV_HOR_RES_MAX;
+	disp_drv.ver_res = LV_VER_RES_MAX;
+	lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+	assert(disp != NULL);
+
+	// Create screen object
+	lv_obj_t *scr = lv_obj_create(NULL);
+	lv_obj_set_size(scr, LV_HOR_RES_MAX, LV_VER_RES_MAX);
+	lv_scr_load(scr);
+
+	// LVGL tick handler
+	tick_thread = new std::thread(&lvgl_tick);
+}
 
 void LVGLDisplay::paintEvent(wxPaintEvent &evt) {
 	wxPaintDC dc(this);
@@ -24,6 +45,8 @@ void LVGLDisplay::render(wxDC &dc) {
 	// TODO: Draw stored data
 }
 
+// ========================= LVGL Driver Functions ========================= //
+
 void LVGLDisplay::lvgl_flush_cb(
     lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p
 ) {
@@ -37,4 +60,14 @@ void LVGLDisplay::lvgl_flush_cb(
 	}
 
 	lv_disp_flush_ready(disp_drv);
+}
+
+void LVGLDisplay::lvgl_tick() {
+	using namespace std::chrono_literals;
+
+	while (true) {
+		std::this_thread::sleep_for(2ms);
+		lv_tick_inc(2);
+		lv_task_handler();
+	}
 }
