c74c4cd72cfb429527dc3f84ff9dd3c57cb4d745
Thurston Yates <thurstonalexyates@gmail.com>
2024-06-26 12:58:07 -0400

    Fix only drawing to first column
    X was not being offset

--- frontend/src/drivers.cpp
+++ frontend/src/drivers.cpp
@@ -27,6 +27,7 @@ void flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_
 			p.Red() = (int32_t)color_p->ch.red;
 			p.Green() = (int32_t)color_p->ch.blue;
 			p.Blue() = (int32_t)color_p->ch.blue;
+			p.OffsetX(data, 1);
 		}
 
 		p = rowStart;
