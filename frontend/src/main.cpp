ee601a460d8467106483605d039b84f4c2334e7f
Thurston Yates <thurstonalexyates@gmail.com>
2024-07-23 12:32:00 -0500

    Remove wxWidgets and install SDL2

--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -1,24 +1,4 @@
-#include "ticktimer.hpp"
-#include "widgets/frame.hpp"
-#include <wx/wx.h>
 
-class App : public wxApp {
-  public:
-	virtual bool OnInit();
-};
+__attribute__((constructor(101))) void sim_init() { return; }
 
-// TODO: PROS uses __attribute__((constructor(101))) to initialize everything (including LVGL)
-// before globals and stuff. The LVGL driver should be reimplemented in a way to allow this to
-// happen, since you can't really create a wxwidget before wxwidgets starts.
-
-bool App::OnInit() {
-	lv_init();
-	TickTimer *tick = new TickTimer();
-	tick->start();
-
-	MainFrame *frame = new MainFrame();
-	frame->Show(true);
-	return true;
-}
-
-wxIMPLEMENT_APP(App);
+int main() { return 0; }
--- frontend/src/ticktimer.cpp
+++
@@ -1,8 +0,0 @@
-#include "ticktimer.hpp"
-
-TickTimer::TickTimer() : wxTimer() {}
-void TickTimer::start() { wxTimer::Start(2); }
-void TickTimer::Notify() {
-	lv_tick_inc(2);
-	lv_timer_handler();
-}
