22b9ced2238e447cbf421b004df1c4528bccf9a6
Thurston Yates <thurstonalexyates@gmail.com>
2024-07-30 11:30:44 -0500

    Comment out weak definitions

--- frontend/src/userfns.cpp
+++ frontend/src/userfns.cpp
@@ -1,7 +1,8 @@
 #include "userfns.hpp"
 
-__attribute__((weak)) void autonomous(void) {}
-__attribute__((weak)) void initialize(void) {}
-__attribute__((weak)) void disabled(void) {}
-__attribute__((weak)) void competition_initialize(void) {}
-__attribute__((weak)) void opcontrol(void) {}
+// FIXME: Always overrides the non-weak functions
+// __attribute__((weak)) void autonomous(void) {}
+// __attribute__((weak)) void initialize(void) {}
+// __attribute__((weak)) void disabled(void) {}
+// __attribute__((weak)) void competition_initialize(void) {}
+// __attribute__((weak)) void opcontrol(void) {}
