f916e55bd0042c74b295248698d97ee2cb9eb40b
Thurston Yates <thurstonalexyates@gmail.com>
2024-08-04 23:00:00 -0500

    Remove window size

--- frontend/src/lvgl_driver.cpp
+++ frontend/src/lvgl_driver.cpp
@@ -92,7 +92,6 @@ void init_lvgl() {
 
 void show_lvgl_window() {
 	ImGui::Begin("Brain Screen", NULL, ImGuiWindowFlags_NoResize);
-	ImGui::SetWindowSize({496, 276});
 	ImGui::Image((void *)texture, {480, 240});
 
 	if (ImGui::IsItemHovered()) {
